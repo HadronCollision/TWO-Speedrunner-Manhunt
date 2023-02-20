bd8ffb8035545ac168f7488de0d0d80affda401a
BuildTools <unconfigured@null.spigotmc.org>
2023-02-20 12:59:01 +0500

    Initial commit

---
+++ src/main/java/net/kaupenjoe/manhunt/Manhunt.java
@@ -0,0 +1,15 @@
+package net.kaupenjoe.manhunt;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Manhunt implements ModInitializer {
+	public static final String MOD_ID = "manhunt";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/kaupenjoe/manhunt/ManhuntClient.java
@@ -0,0 +1,10 @@
+package net.kaupenjoe.manhunt;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ManhuntClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
